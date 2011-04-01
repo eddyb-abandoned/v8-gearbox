951475bf07001b5cd6fe1f3d508d75d18d0cc82f
Eduard Burtescu <eddy_me08@yahoo.com>
2011-04-01 09:39:06 +0300

    Remove version().

--- src/global.cc
+++ src/global.cc
@@ -16,8 +16,7 @@ using namespace Gearbox;
 #include "modules/GL.h"
 
 v8::Handle<v8::Value> __global_print(const v8::Arguments& args) {
-    if(args.Length())
-    {
+    if(args.Length()) {
         for(int i = 0; i < args.Length(); i++)
             printf("%s\n", *Value(args[i]).to<String>());
         return undefined;
@@ -26,9 +25,7 @@ v8::Handle<v8::Value> __global_print(const v8::Arguments& args) {
 }
 
 v8::Handle<v8::Value> __global_load(const v8::Arguments& args) {
-    if(args.Length() >= 1)
-    {
-        v8::HandleScope handle_scope;
+    if(args.Length() >= 1) {
         String file = Value(args[0]);
         if(file.empty())
             return Throw(Error("Error loading file"));
@@ -45,16 +42,11 @@ v8::Handle<v8::Value> __global_exit(const v8::Arguments& args) {
     return undefined;
 }
 
-v8::Handle<v8::Value> __global_version(const v8::Arguments& args) {
-    return String(v8::V8::GetVersion());
-}
-
 void SetupGlobal(v8::Handle<v8::Object> global) {
 #define GEARBOX_SET_FUNCTION(base, func) base->Set(String(#func), Function(__##base##_##func, #func))
     GEARBOX_SET_FUNCTION(global, print);
     GEARBOX_SET_FUNCTION(global, load);
     GEARBOX_SET_FUNCTION(global, exit);
-    GEARBOX_SET_FUNCTION(global, version);
 #undef GEARBOX_SET_FUNCTION
     
     global->Set(String("global"), global);
