702c4d6af3afc106e95902e28429e76fb9885f79
Sanjeev BA <iamsanjeev@gmail.com>
2011-09-04 01:36:21 +0900

    Add photo widget.

--- src/modules/Elm.cc
+++ src/modules/Elm.cc
@@ -3822,10 +3822,248 @@ static void _Elm_ProgressBar_inverted(v8::Local<v8::String>, v8::Local<v8::Value
     elm_progressbar_inverted_set(This["eo"], (Eina_Bool)(_value.to<int>()));
 }
 
-static v8::Handle<v8::Value> _Elm_Entry_Entry(const v8::Arguments &args) {
+static v8::Handle<v8::Value> _Elm_Photo_Photo(const v8::Arguments &args) {
     Value This(args.This());
     if(args.Length() >= 1) {
         #line 861 "src/modules/Elm.gear"
+        Value parent(args[0]);
+        This["eo"] = elm_photo_add(parent["eo"]);
+        evas_object_show(This["eo"]);
+        return undefined;
+    }
+    THROW_ERROR("Invalid call to Elm.Photo");
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_x(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 47 "src/modules/Elm.gear"
+    Evas_Coord x, y, width, height;
+    evas_object_geometry_get(This["eo"], &x, &y, &width, &height);
+    return Number(x);
+}
+
+static void _Elm_Photo_x(v8::Local<v8::String>, v8::Local<v8::Value> __x, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 52 "src/modules/Elm.gear"
+    Value _x(__x);
+    Evas_Coord x, y, width, height;
+    evas_object_geometry_get(This["eo"], &x, &y, &width, &height);
+    evas_object_move(This["eo"], _x, y);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_y(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 59 "src/modules/Elm.gear"
+    Evas_Coord x, y, width, height;
+    evas_object_geometry_get(This["eo"], &x, &y, &width, &height);
+    return Number(y);
+}
+
+static void _Elm_Photo_y(v8::Local<v8::String>, v8::Local<v8::Value> __y, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 64 "src/modules/Elm.gear"
+    Value _y(__y);
+    Evas_Coord x, y, width, height;
+    evas_object_geometry_get(This["eo"], &x, &y, &width, &height);
+    evas_object_move(This["eo"], x, _y);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_height(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 71 "src/modules/Elm.gear"
+    Evas_Coord x, y, width, height;
+    evas_object_geometry_get(This["eo"], &x, &y, &width, &height);
+    return Number(height);
+}
+
+static void _Elm_Photo_height(v8::Local<v8::String>, v8::Local<v8::Value> __height, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 76 "src/modules/Elm.gear"
+    Value _height(__height);
+    Evas_Coord x, y, width, height;
+    evas_object_geometry_get(This["eo"], &x, &y, &width, &height);
+    evas_object_resize(This["eo"], width, _height);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_width(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 83 "src/modules/Elm.gear"
+    Evas_Coord x, y, width, height;
+    evas_object_geometry_get(This["eo"], &x, &y, &width, &height);
+    return Number(width);
+}
+
+static void _Elm_Photo_width(v8::Local<v8::String>, v8::Local<v8::Value> __width, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 88 "src/modules/Elm.gear"
+    Value _width(__width);
+    Evas_Coord x, y, width, height;
+    evas_object_geometry_get(This["eo"], &x, &y, &width, &height);
+    evas_object_resize(This["eo"], _width, height);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_label(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 95 "src/modules/Elm.gear"
+    return String(elm_object_text_get(This["eo"]));
+}
+
+static void _Elm_Photo_label(v8::Local<v8::String>, v8::Local<v8::Value> _label, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 98 "src/modules/Elm.gear"
+    Value label(_label);
+    elm_object_text_set(This["eo"], label.to<String>());
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_text(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 103 "src/modules/Elm.gear"
+    return String(elm_object_text_get(This["eo"]));
+}
+
+static void _Elm_Photo_text(v8::Local<v8::String>, v8::Local<v8::Value> _text, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 106 "src/modules/Elm.gear"
+    Value text(_text);
+    elm_object_text_set(This["eo"], text.to<String>());
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_scale(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 111 "src/modules/Elm.gear"
+    return Number(elm_object_scale_get(This["eo"]));
+}
+
+static void _Elm_Photo_scale(v8::Local<v8::String>, v8::Local<v8::Value> _scale, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 114 "src/modules/Elm.gear"
+    Value scale(_scale);
+    elm_object_scale_set(This["eo"], scale);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_xalign(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 119 "src/modules/Elm.gear"
+    double x, y;
+    evas_object_size_hint_align_get(This["eo"], &x, &y);
+    return Number(x);
+}
+
+static void _Elm_Photo_xalign(v8::Local<v8::String>, v8::Local<v8::Value> __x, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 124 "src/modules/Elm.gear"
+    Value _x(__x);
+    double x, y;
+    evas_object_size_hint_align_get(This["eo"], &x, &y);
+    evas_object_size_hint_align_set(This["eo"], _x, y);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_yalign(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 131 "src/modules/Elm.gear"
+    double x, y;
+    evas_object_size_hint_align_get(This["eo"], &x, &y);
+    return Number(y);
+}
+
+static void _Elm_Photo_yalign(v8::Local<v8::String>, v8::Local<v8::Value> __y, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 136 "src/modules/Elm.gear"
+    Value _y(__y);
+    double x, y;
+    evas_object_size_hint_align_get(This["eo"], &x, &y);
+    evas_object_size_hint_align_set(This["eo"], x, _y);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_style(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 143 "src/modules/Elm.gear"
+    return String(elm_object_style_get(This["eo"]));
+}
+
+static void _Elm_Photo_style(v8::Local<v8::String>, v8::Local<v8::Value> _style, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 146 "src/modules/Elm.gear"
+    Value style(_style);
+    elm_object_style_set(This["eo"], style.to<String>());
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_visible(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 151 "src/modules/Elm.gear"
+    return Value(evas_object_visible_get(This["eo"]));
+}
+
+static void _Elm_Photo_visible(v8::Local<v8::String>, v8::Local<v8::Value> _visible, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 154 "src/modules/Elm.gear"
+    Value visible(_visible);
+    if(visible)
+        evas_object_show(This["eo"]);
+    else
+        evas_object_hide(This["eo"]);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_image(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 867 "src/modules/Elm.gear"
+    // TODO : Not implemented in EFL
+    return String("");
+}
+
+static void _Elm_Photo_image(v8::Local<v8::String>, v8::Local<v8::Value> __image, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 871 "src/modules/Elm.gear"
+    Value _image(__image);
+    elm_photo_file_set(This["eo"], _image);
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_size(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 876 "src/modules/Elm.gear"
+    // TODO : Not implemented in EFL
+    return Integer(0);
+}
+
+static void _Elm_Photo_size(v8::Local<v8::String>, v8::Local<v8::Value> __value, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 880 "src/modules/Elm.gear"
+    Value _value(__value);
+    elm_photo_size_set(This["eo"], (_value.to<int>()));
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_fill(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 885 "src/modules/Elm.gear"
+    // TODO : Not implemented in EFL
+    return Integer(0);
+}
+
+static void _Elm_Photo_fill(v8::Local<v8::String>, v8::Local<v8::Value> __value, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 889 "src/modules/Elm.gear"
+    Value _value(__value);
+    elm_photo_fill_inside_set(This["eo"], (Eina_Bool)(_value.to<int>()));
+}
+
+static v8::Handle<v8::Value> _Elm_Photo_editable(v8::Local<v8::String>, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 894 "src/modules/Elm.gear"
+    // TODO : Not implemented in EFL
+    return Integer(0);
+}
+
+static void _Elm_Photo_editable(v8::Local<v8::String>, v8::Local<v8::Value> __value, const v8::AccessorInfo &args) {
+    Value This(args.This());
+    #line 898 "src/modules/Elm.gear"
+    Value _value(__value);
+    elm_photo_editable_set(This["eo"], (Eina_Bool)(_value.to<int>()));
+}
+
+static v8::Handle<v8::Value> _Elm_Entry_Entry(const v8::Arguments &args) {
+    Value This(args.This());
+    if(args.Length() >= 1) {
+        #line 906 "src/modules/Elm.gear"
         Value parent(args[0]);
         This["eo"] = elm_entry_add(parent["eo"]);
         evas_object_show(This["eo"]);
@@ -4006,7 +4244,7 @@ static void _Elm_Entry_visible(v8::Local<v8::String>, v8::Local<v8::Value> _visi
 
 static v8::Handle<v8::Value> _Elm_Entry_entry(v8::Local<v8::String>, const v8::AccessorInfo &args) {
     Value This(args.This());
-    #line 872 "src/modules/Elm.gear"
+    #line 917 "src/modules/Elm.gear"
     const char *entry;
     entry = elm_entry_entry_get(This["eo"]);
     return String(entry);
@@ -4014,7 +4252,7 @@ static v8::Handle<v8::Value> _Elm_Entry_entry(v8::Local<v8::String>, const v8::A
 
 static void _Elm_Entry_entry(v8::Local<v8::String>, v8::Local<v8::Value> _text, const v8::AccessorInfo &args) {
     Value This(args.This());
-    #line 866 "src/modules/Elm.gear"
+    #line 911 "src/modules/Elm.gear"
     Value text(_text);
     This["entry"] = text;
     elm_entry_entry_set(This["eo"], text);
@@ -4032,7 +4270,7 @@ static v8::Handle<v8::Value> _Elm_toString(const v8::Arguments &args) {
 }
 
 
-#line 4035 "src/modules/Elm.cc"
+#line 4273 "src/modules/Elm.cc"
 static void _setup_Elm(Value _exports) {
     v8::Handle<v8::FunctionTemplate> _Elm_EvasObject = v8::FunctionTemplate::New(_Elm_EvasObject_EvasObject);
     _Elm_EvasObject->SetClassName(String("EvasObject"));
@@ -4320,6 +4558,24 @@ static void _setup_Elm(Value _exports) {
     _Elm_ProgressBar->PrototypeTemplate()->SetAccessor(String("horizontal"), _Elm_ProgressBar_horizontal, _Elm_ProgressBar_horizontal);
     _Elm_ProgressBar->PrototypeTemplate()->SetAccessor(String("inverted"), _Elm_ProgressBar_inverted, _Elm_ProgressBar_inverted);
     _exports["ProgressBar"] = _Elm_ProgressBar->GetFunction();
+    v8::Handle<v8::FunctionTemplate> _Elm_Photo = v8::FunctionTemplate::New(_Elm_Photo_Photo);
+    _Elm_Photo->SetClassName(String("Photo"));
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("x"), _Elm_Photo_x, _Elm_Photo_x);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("y"), _Elm_Photo_y, _Elm_Photo_y);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("height"), _Elm_Photo_height, _Elm_Photo_height);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("width"), _Elm_Photo_width, _Elm_Photo_width);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("label"), _Elm_Photo_label, _Elm_Photo_label);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("text"), _Elm_Photo_text, _Elm_Photo_text);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("scale"), _Elm_Photo_scale, _Elm_Photo_scale);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("xalign"), _Elm_Photo_xalign, _Elm_Photo_xalign);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("yalign"), _Elm_Photo_yalign, _Elm_Photo_yalign);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("style"), _Elm_Photo_style, _Elm_Photo_style);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("visible"), _Elm_Photo_visible, _Elm_Photo_visible);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("image"), _Elm_Photo_image, _Elm_Photo_image);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("size"), _Elm_Photo_size, _Elm_Photo_size);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("fill"), _Elm_Photo_fill, _Elm_Photo_fill);
+    _Elm_Photo->PrototypeTemplate()->SetAccessor(String("editable"), _Elm_Photo_editable, _Elm_Photo_editable);
+    _exports["Photo"] = _Elm_Photo->GetFunction();
     v8::Handle<v8::FunctionTemplate> _Elm_Entry = v8::FunctionTemplate::New(_Elm_Entry_Entry);
     _Elm_Entry->SetClassName(String("Entry"));
     _Elm_Entry->PrototypeTemplate()->SetAccessor(String("x"), _Elm_Entry_x, _Elm_Entry_x);
